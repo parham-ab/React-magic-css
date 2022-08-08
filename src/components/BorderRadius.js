6c9fd7e5a99d2527351389e18e2f3b35442e8b5c
Parham Abolghasemi <parhamab17@gmail.com>
2022-08-08 08:55:21 +0430

    conditions applied

--- src/components/BorderRadius.js
+++ src/components/BorderRadius.js
@@ -3,9 +3,13 @@ import { useState } from "react";
 import {
   Box,
   FormControl,
+  FormControlLabel,
+  FormLabel,
   Grid,
   InputLabel,
   MenuItem,
+  Radio,
+  RadioGroup,
   Select,
   Slider,
   Typography,
@@ -15,6 +19,12 @@ import { Container } from "@mui/system";
 const BorderRadius = () => {
   const [type, setType] = useState("Same on all sides");
   const [allRadius, setAllRadius] = useState(0);
+  const [unit, setUnit] = useState("px");
+  // each sides radius
+  const [topLeft, setTopLeft] = useState(0);
+  const [topRight, setTopRight] = useState(0);
+  const [bottomRight, setBottomRight] = useState(0);
+  const [bottomLeft, setBottomLeft] = useState(0);
 
   return (
     <Container>
@@ -35,27 +45,154 @@ const BorderRadius = () => {
               <MenuItem value={"Same on all sides"}>Same on all sides</MenuItem>
             </Select>
           </FormControl>
-          <Typography
-            variant="h6"
-            fontWeight="bold"
-            color="text.secondary"
-            textAlign="center"
-          >
-            Radius
-          </Typography>
-          <Slider
-            min={0}
-            max={100}
-            size="small"
-            defaultValue={0}
-            aria-label="Small"
-            valueLabelDisplay="auto"
-            sx={{ color: "#2764b0" }}
-            value={allRadius}
-            onChange={(e) => setAllRadius(e.target.value)}
-          />
+          {/* same on all sides */}
+          {type === "Same on all sides" ? (
+            <>
+              <Typography
+                variant="h6"
+                fontWeight="bold"
+                color="text.secondary"
+                textAlign="center"
+              >
+                Radius
+              </Typography>
+              <Slider
+                min={0}
+                max={unit === "px" ? 100 : 50}
+                size="small"
+                defaultValue={0}
+                aria-label="Small"
+                valueLabelDisplay="auto"
+                sx={{ color: "#2764b0" }}
+                value={allRadius}
+                onChange={(e) => setAllRadius(e.target.value)}
+              />
+
+              <FormControl>
+                <FormLabel id="unit" color="success">
+                  Unit
+                </FormLabel>
+                <RadioGroup
+                  name="unit"
+                  value={unit}
+                  onChange={(e) => setUnit(e.target.value)}
+                >
+                  <FormControlLabel
+                    value="px"
+                    control={<Radio color="success" size="small" />}
+                    label="PX"
+                  />
+                  <FormControlLabel
+                    value="%"
+                    control={<Radio color="success" size="small" />}
+                    label="%"
+                  />
+                </RadioGroup>
+              </FormControl>
+            </>
+          ) : (
+            <>
+              <Typography
+                variant="h6"
+                fontWeight="bold"
+                color="text.secondary"
+                textAlign="center"
+              >
+                Top-left
+              </Typography>
+              <Slider
+                min={0}
+                max={unit === "px" ? 100 : 50}
+                size="small"
+                defaultValue={0}
+                aria-label="Small"
+                valueLabelDisplay="auto"
+                sx={{ color: "#2764b0" }}
+                value={topLeft}
+                onChange={(e) => setTopLeft(e.target.value)}
+              />
+              <Typography
+                variant="h6"
+                fontWeight="bold"
+                color="text.secondary"
+                textAlign="center"
+              >
+                Top-right
+              </Typography>
+              <Slider
+                min={0}
+                max={unit === "px" ? 100 : 50}
+                size="small"
+                defaultValue={0}
+                aria-label="Small"
+                valueLabelDisplay="auto"
+                sx={{ color: "#2764b0" }}
+                value={topRight}
+                onChange={(e) => setTopRight(e.target.value)}
+              />
+              <Typography
+                variant="h6"
+                fontWeight="bold"
+                color="text.secondary"
+                textAlign="center"
+              >
+                Bottom-right
+              </Typography>
+              <Slider
+                min={0}
+                max={unit === "px" ? 100 : 50}
+                size="small"
+                defaultValue={0}
+                aria-label="Small"
+                valueLabelDisplay="auto"
+                sx={{ color: "#2764b0" }}
+                value={bottomRight}
+                onChange={(e) => setBottomRight(e.target.value)}
+              />
+              <Typography
+                variant="h6"
+                fontWeight="bold"
+                color="text.secondary"
+                textAlign="center"
+              >
+                Bottom-left
+              </Typography>
+              <Slider
+                min={0}
+                max={unit === "px" ? 100 : 50}
+                size="small"
+                defaultValue={0}
+                aria-label="Small"
+                valueLabelDisplay="auto"
+                sx={{ color: "#2764b0" }}
+                value={bottomLeft}
+                onChange={(e) => setBottomLeft(e.target.value)}
+              />
+
+              <FormControl>
+                <FormLabel id="unit" color="success">
+                  Unit
+                </FormLabel>
+                <RadioGroup
+                  name="unit"
+                  value={unit}
+                  onChange={(e) => setUnit(e.target.value)}
+                >
+                  <FormControlLabel
+                    value="px"
+                    control={<Radio color="success" size="small" />}
+                    label="PX"
+                  />
+                  <FormControlLabel
+                    value="%"
+                    control={<Radio color="success" size="small" />}
+                    label="%"
+                  />
+                </RadioGroup>
+              </FormControl>
+            </>
+          )}
         </Grid>
-        {/* same on all sides */}
         {/* <Grid item></Grid> */}
         {/* different on all sides */}
         {/* <Grid item></Grid> */}
@@ -65,9 +202,20 @@ const BorderRadius = () => {
           <Box
             component="div"
             className="radiusbox"
-            sx={{ borderRadius: `${allRadius}px` }}
+            sx={{
+              borderRadius:
+                type === "Same on all sides" && unit === "px"
+                  ? `${allRadius}px`
+                  : type === "Same on all sides" && unit === "%"
+                  ? `${allRadius}%`
+                  : type === "Different on all Sides" && unit === "px"
+                  ? `${topLeft}px ${topRight}px ${bottomRight}px ${bottomLeft}px`
+                  : type === "Different on all Sides" && unit === "%"
+                  ? `${topLeft}% ${topRight}% ${bottomRight}% ${bottomLeft}%`
+                  : "",
+              // borderRadius: unit === "px" ? `${allRadius}px` : `${allRadius}%`,
+            }}
           ></Box>
-          {console.log(allRadius)}
         </Grid>
       </Grid>
     </Container>
