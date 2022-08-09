ca4dfbe3f58e624ce8207cb9ce2e3482a6885f27
Parham Abolghasemi <parhamab17@gmail.com>
2022-08-09 10:00:36 +0430

    all sliders created

--- src/components/ImageFilter.js
+++ src/components/ImageFilter.js
@@ -23,8 +23,8 @@ const ImageFilter = () => {
   const [opacityVal, setOpacityVal] = useState(0);
   const [brightnessVal, setBrightnessVal] = useState(0);
   const [contrastVal, setContrastVal] = useState(0);
-  const [HueVal, setHueVal] = useState(0);
-  const [invert, setInvertVal] = useState(0);
+  const [hueVal, setHueVal] = useState(0);
+  const [invertVal, setInvertVal] = useState(0);
   // dynamic title
   DynamicTitle("Magic CSS - Image filter");
 
@@ -39,7 +39,7 @@ const ImageFilter = () => {
             min={0}
             max={100}
             size="small"
-            defaultValue={50}
+            defaultValue={0}
             aria-label="Small"
             valueLabelDisplay="auto"
             sx={{ color: "#2764b0" }}
@@ -60,6 +60,99 @@ const ImageFilter = () => {
             sx={{ color: "#2764b0" }}
             value={blurVal}
             onChange={(e) => setBlurVal(e.target.value)}
+          />
+
+          <Typography variant="h5" color="text.secondary" textAlign="center">
+            Sepia
+          </Typography>
+          <Slider
+            min={0}
+            max={1}
+            step={0.1}
+            size="small"
+            defaultValue={0}
+            aria-label="Small"
+            valueLabelDisplay="auto"
+            sx={{ color: "#2764b0" }}
+            value={sepiaVal}
+            onChange={(e) => setSepiaVal(e.target.value)}
+          />
+
+          <Typography variant="h5" color="text.secondary" textAlign="center">
+            Saturate
+          </Typography>
+          <Slider
+            min={0}
+            max={1}
+            step={0.1}
+            size="small"
+            defaultValue={0}
+            aria-label="Small"
+            valueLabelDisplay="auto"
+            sx={{ color: "#2764b0" }}
+            value={saturateVal}
+            onChange={(e) => setSaturateVal(e.target.value)}
+          />
+
+          <Typography variant="h5" color="text.secondary" textAlign="center">
+            Opacity
+          </Typography>
+          <Slider
+            min={0}
+            max={1}
+            step={0.1}
+            size="small"
+            defaultValue={0}
+            aria-label="Small"
+            valueLabelDisplay="auto"
+            sx={{ color: "#2764b0" }}
+            value={opacityVal}
+            onChange={(e) => setOpacityVal(e.target.value)}
+          />
+
+          <Typography variant="h5" color="text.secondary" textAlign="center">
+            Contrast
+          </Typography>
+          <Slider
+            min={0}
+            max={200}
+            size="small"
+            defaultValue={100}
+            aria-label="Small"
+            valueLabelDisplay="auto"
+            sx={{ color: "#2764b0" }}
+            value={contrastVal}
+            onChange={(e) => setContrastVal(e.target.value)}
+          />
+
+          <Typography variant="h5" color="text.secondary" textAlign="center">
+            Hue-rotate
+          </Typography>
+          <Slider
+            min={0}
+            max={360}
+            size="small"
+            defaultValue={0}
+            aria-label="Small"
+            valueLabelDisplay="auto"
+            sx={{ color: "#2764b0" }}
+            value={hueVal}
+            onChange={(e) => setHueVal(e.target.value)}
+          />
+
+          <Typography variant="h5" color="text.secondary" textAlign="center">
+            Invert
+          </Typography>
+          <Slider
+            min={0}
+            max={200}
+            size="small"
+            defaultValue={0}
+            aria-label="Small"
+            valueLabelDisplay="auto"
+            sx={{ color: "#2764b0" }}
+            value={invertVal}
+            onChange={(e) => setInvertVal(e.target.value)}
           />
         </Grid>
         <Grid item mt={20}>
