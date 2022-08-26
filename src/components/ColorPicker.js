e75d5ee3f86b7af91cc73d09b24e49c6716b356e
Parham Abolghasemi <parhamab17@gmail.com>
2022-08-26 13:18:20 +0430

    color picker logic section done

--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import { Button } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { ChromePicker, SketchPicker } from "react-color";
+// icon
+import AssignmentIcon from "@mui/icons-material/Assignment";
+// copy to clipboard button
+import copy from "copy-to-clipboard";
 
 const ColorPicker = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const [firstColor, setFirstColor] = useState("");
+  const [finalSource, setFinalSource] = useState();
+  // change hanlder
+  const changeHandle = (e) => {
+    setFirstColor(e.hex);
+  };
+  // copy to clipboard function
+  const copyToClipboard = () => {
+    copy(finalSource);
+  };
+  useEffect(() => {
+    setFinalSource(firstColor);
+  }, [firstColor]);
+
+  return (
+    <div>
+      <SketchPicker color={firstColor} onChangeComplete={changeHandle} />
+      <Button
+        onClick={copyToClipboard}
+        sx={{ marginY: "50px" }}
+        variant="contained"
+        startIcon={<AssignmentIcon />}
+      >
+        Copy
+      </Button>
+    </div>
+  );
 };
 
 export default ColorPicker;
